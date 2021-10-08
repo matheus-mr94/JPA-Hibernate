6c4f086a58793ede823406f551e91ae654888e24
Matheus Medeiros <mmr94@live.com>
2021-10-08 14:48:22 -0300

    Consultando entidades utilizando JPQL

--- src/main/java/br/com/alura/loja/dao/ProdutoDao.java
+++ src/main/java/br/com/alura/loja/dao/ProdutoDao.java
@@ -1,19 +1,37 @@
 package br.com.alura.loja.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import br.com.alura.loja.modelo.Produto;
 
 public class ProdutoDao {
-	
+
 	private EntityManager em;
 
 	public ProdutoDao(EntityManager em) {
 		this.em = em;
 	}
-	
+
 	public void cadastrar(Produto produto) {
 		this.em.persist(produto);
-		
+	}
+
+	public void atualizar(Produto produto) {
+		this.em.merge(produto);
+	}
+	public void remover(Produto produto) {
+		produto = em.merge(produto); // garantir que esteja no estado MANAGED
+		this.em.remove(produto);
+	}
+	
+	public Produto bucarPorId(Long id) {
+		return em.find(Produto.class, id);
+	}
+	
+	public List<Produto> buscarTodos(){
+		String jpql = "SELECT p FROM Produto p ";
+		return em.createQuery(jpql, Produto.class).getResultList();
 	}
 }
--- src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -1,6 +1,7 @@
 package br.com.alura.loja.testes;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 
@@ -12,24 +13,35 @@ import br.com.alura.loja.util.JPAUtil;
 
 public class CadastroDeProduto {
 	public static void main(String[] args) {
-		Categoria celulares = new Categoria("CELULARES");
-		Produto celular = new Produto("Xiaomi Redmi","Muito legal",new BigDecimal("800"),celulares);
-		
+		cadastrarProduto();
 		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDao produtoDao = new ProdutoDao(em);
 		
+		Produto p = produtoDao.bucarPorId(1l);
+		System.out.println(p.getPreco());
+		
+		List<Produto> todos = produtoDao.buscarTodos();
+		todos.forEach(p2 -> System.out.println(p.getNome()));
+
+	}
+
+	private static void cadastrarProduto() {
+		Categoria celulares = new Categoria("CELULARES");
+		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares);
+
+		EntityManager em = JPAUtil.getEntityManager();
+
 		ProdutoDao produtoDao = new ProdutoDao(em);
 		CategoriaDao categoriaDao = new CategoriaDao(em);
-		
-		
-		//Utilizado quando não temos um controle de transação automático
+
+		// Utilizado quando não temos um controle de transação automático
 		em.getTransaction().begin();
-		
+
 		categoriaDao.cadastrar(celulares);
 		produtoDao.cadastrar(celular);
-		
+
 		em.getTransaction().commit();
 		em.close();
 	}
-	
 
 }
