b0438d5978525f965744dfd411d67ee196c66422
Matheus Medeiros <mmr94@live.com>
2021-10-13 16:10:22 -0300

    Simplificando entidades com Embeddable

--- src/main/java/br/com/alura/loja/modelo/Cliente.java
+++ src/main/java/br/com/alura/loja/modelo/Cliente.java
@@ -1,5 +1,6 @@
 package br.com.alura.loja.modelo;
 
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,33 +14,18 @@ public class Cliente {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String nome;
-	private String cpf;
+	
+	@Embedded
+	private DadosPessoais dadosPessoais;
 
 	public Cliente() {
 	}
 
 	public Cliente(String nome, String cpf) {
-		this.nome = nome;
-		this.cpf = cpf;
+		this.dadosPessoais = new DadosPessoais(nome, cpf);
 	}
 
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
+	
 	public Long getId() {
 		return id;
 	}
@@ -47,5 +33,17 @@ public class Cliente {
 	public void setId(Long id) {
 		this.id = id;
 	}
-
+	
+	public DadosPessoais getDadosPessoais() {
+		return dadosPessoais;
+	}
+	
+	//Método Delegate, deleta a chamada do nome para o atributo dados pessoais
+	public String getNome() {
+		return this.dadosPessoais.getNome();
+	}
+	
+	public String getCpf() {
+		return this.dadosPessoais.getCpf();
+	}
 }
